788fdef90aa61c2baf7a82851f9fb76cde0f13b9
DaveKeith <d.keith78@yahoo.com>
2019-01-31 08:59:45 -0500

    Updates

---
+++ precache-manifest.7b31159ec69575f1de27197761daf686.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b9f9603f74507ac8517e0f29d37709d",
+    "url": "/static/media/the-blends-2-1156219.1b9f9603.jpg"
+  },
+  {
+    "revision": "e33d792a4c42d2c69df6dee3ca6da6c3",
+    "url": "/static/media/davekeithpic.e33d792a.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b81ea9a502976e92445",
+    "url": "/static/js/main.9b81ea9a.chunk.js"
+  },
+  {
+    "revision": "ed5bc8e1720f62a6e2e8",
+    "url": "/static/js/1.ed5bc8e1.chunk.js"
+  },
+  {
+    "revision": "9b81ea9a502976e92445",
+    "url": "/static/css/main.f509029b.chunk.css"
+  },
+  {
+    "revision": "c3c65c2c089b5bc66d26f8985e79b7ca",
+    "url": "/index.html"
+  }
+];
